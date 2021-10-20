d279627fae68c0762496c0d8b2fe8d4ef0c99cc6
Diana-Dyakovskaya <diana.dyakovskaya7@gmail.com>
2021-10-20 14:42:07 +0300

    add carousel

--- src/app/scripts/shared/app.js
+++ src/app/scripts/shared/app.js
@@ -55,6 +55,28 @@ export const app = () => {
       sliderTrack.style.background = `linear-gradient(to right, #f1f2f4 ${percent1}%, #151C22 ${percent1}%, #151C22 ${percent2}%, #f1f2f4 ${percent2}%)`
     }
     fillColor()
+    // carousel
+    const widthView = document.querySelector('.carousel__view').offsetWidth
+    const carouselLine = document.querySelector('.carousel__line')
+    const btnPrev = document.querySelector('.js-btn-prev')
+    const btnNext = document.querySelector('.js-btn-next')
+    let slide = 0
+    btnNext.addEventListener('click', (event) => {
+      if (slide < carouselLine.length) {
+        slide += 1
+      } else {
+        slide = 0
+      }
+      carouselLine.style.left = -slide * widthView + 'px'
+    })
+    btnPrev.addEventListener('click', (event) => {
+      if (slide > 0) {
+        slide -= 1
+      } else {
+        slide = carouselLine.length - 1
+      }
+      carouselLine.style.left = -slide * widthView + 'px'
+    })
     // active
     const colorItems = document.querySelector('.color')
     colorItems.addEventListener('click', (event) => {
