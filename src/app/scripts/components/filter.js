f9cc542e0e7d2f09a8791152abe42202a51d91aa
ereburg <gimmethenick@gmail.com>
2021-10-21 13:40:46 +0300

    migrate Filter script to class syntax, disable other scripts

--- src/app/scripts/components/filter.js
+++ src/app/scripts/components/filter.js
@@ -1,22 +1,59 @@
-export function filter() {
-  const filter = document.querySelector('.js-filter')
-  filter.addEventListener('click', (event) => {
-    if (!event.target.closest('.filter__btn')) {
-      return
-    }
-    showAccord(event.target.closest('.js-filter'))
-  })
-  const showAccord = (el) => {
-    el.classList.toggle('visible')
+// export function filter() {
+//   const filter = document.querySelector('.js-filter')
+//   filter.addEventListener('click', (event) => {
+//     if (!event.target.closest('.filter__btn')) {
+//       return
+//     }
+//     showAccord(event.target.closest('.js-filter'))
+//   })
+//   const showAccord = (el) => {
+//     el.classList.toggle('visible')
+//   }
+//   const accordGroup = document.querySelector('.js-accord-group')
+//   accordGroup.addEventListener('click', (event) => {
+//     if (!event.target.closest('.js-accord__btn')) {
+//       return
+//     }
+//     showBody(event.target.closest('.js-accord'))
+//   })
+//   const showBody = (el) => {
+//     el.classList.toggle('active')
+//   }
+// }
+
+export class Filter {
+  constructor(el) {
+    this.$node = el
+    this.$accordGroup = el.querySelector('.js-accord-group')
+
+    this.onFilterNodeClick = this.onFilterNodeClick.bind(this)
+    this.onAccordGroupClick = this.onAccordGroupClick.bind(this)
+
+    this.setup()
   }
-  const accordGroup = document.querySelector('.js-accord-group')
-  accordGroup.addEventListener('click', (event) => {
-    if (!event.target.closest('.js-accord__btn')) {
-      return
-    }
-    showBody(event.target.closest('.js-accord'))
-  })
-  const showBody = (el) => {
-    el.classList.toggle('active')
+
+  setup() {
+    this.$node.addEventListener('click', this.onFilterNodeClick)
+    this.$accordGroup.addEventListener('click', this.onAccordGroupClick)
+  }
+
+  onFilterNodeClick(e) {
+    if (!e.target.closest('.filter__btn')) return
+
+    this.toggleState(e.target.closest('.js-filter'), 'visible')
+  }
+
+  onAccordGroupClick(e) {
+    if (!e.target.closest('.js-accord__btn')) return
+
+    this.toggleState(e.target.closest('.js-accord'), 'active')
+  }
+
+  toggleState(node, classname) {
+    node.classList.toggle(classname)
+  }
+
+  static init(el) {
+    return el && new Filter(el)
   }
 }
